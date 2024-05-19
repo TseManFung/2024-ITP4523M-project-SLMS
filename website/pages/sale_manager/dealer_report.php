31a13fa1fdcd2f7f3531f48ef3b24fcb3ac09c2c
Tse Man Fung <andytse0917@gmail.com>
2024-05-19 14:05:45 +0800

    chore: Update dealer cart CSS and checkout form

--- website/pages/sale_manager/dealer_report.php
+++ website/pages/sale_manager/dealer_report.php
@@ -26,8 +26,7 @@
     <!-- navbar -->
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container-fluid justify-content-center">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse nav-wrap" id="navbarSupportedContent">
@@ -65,16 +64,22 @@
       <br />
       <div class="row row--top-40">
         <div class="col-md-4">
-          <h2 class="row__title">Spare Part Report(2)</h2>
+          <h2 class="row__title">[dealer ID | dealer name]'s Report (2)</h2>
+          # this report genarate at [2024-01-01 00:00:00]
         </div>
         <div class="col">
         </div>
         <div class="col-md-4 position-relative">
           <div class="form-floating position-absolute bottom-0 end-0" style="padding-right: 12px">
             <select class="form-select" id="sort">
-              <option value="NA">Newest Arrivals</option>
-              <option value="PLH">Price: Low to High</option>
-              <option value="PHL">Price: High to Low</option>
+              <option value="N">Newest</option>
+              <option value="O">Oldst</option>
+              <option value="QLH">Quantity: Low to High</option>
+              <option value="QHL">Quantity: High to Low</option>
+              <option value="WLH">Weight: Low to High</option>
+              <option value="WHL">Weight: High to Low</option>
+              <option value="ALH">Amount: Low to High</option>
+              <option value="AHL">Amount: High to Low</option>
             </select>
             <label for="sort">Sort</label>
           </div>
@@ -86,11 +91,11 @@
         <!-- table header -->
         <thead class="table-light table-header">
           <tr>
-            <th scope="col" style="width: 10%;">ID</th>
-            <th scope="col" style="width: 40%;">Name</th>
-            <th scope="col" style="width: 20%;text-align:center;">photo</th>
-            <th scope="col" style="width: 15%;">Total order number</th>
-            <th scope="col" style="width: 15%;">Total sale amount</th>
+            <th scope="col">Order ID</th>
+            <th scope="col">Order Date & Time</th>
+            <th scope="col">Total Item Quantity</th>
+            <th scope="col">Total Item Weight</th>
+            <th scope="col">Total Order Amount</th>
           </tr>
         </thead>
         <!-- /table header -->
@@ -98,36 +103,19 @@
         <tbody>
           <!-- record -->
           <tr>
-            <th scope="row">100001</th>
-            <td>idk</td>
-            <td>
-              <div class="table-img-box center-LR center-TB">
-                <img class="table-img" src="../../images/item/100001.jpg" />
-              </div>
-            </td>
+            <th scope="row">0123456789</th>
+            <td>2024-01-01 00:00:00</td>
             <td>12</td>
+            <td>1.23kg</td>
             <td>$123456</td>
           </tr>
           <!-- /record -->
           <!-- record -->
           <tr>
-            <th scope="row">200002</th>
-            <td>a long long long long long long long long long long long ng long long long long long long ng long long
-              long long long long ng long long long long long long ng long long long long long long ng long long long
-              long long long ng long long long long long long ng long long long long long long ng long long long long
-              long long ng long long long long long long ng long long long long long long ng long long long long long
-              long ng long long long long long long ng long long long long long long ng long long long long long long ng
-              long long long long long long ng long long long long long long ng long long long long long long ng long
-              long long long long long ng long long long long long long long long long long long long longg long long
-              long long long long long long long long long long long lon long long long long long long long long long
-              long long long long long long long long long long long long long long long long long long long long long
-              name</td>
-            <td>
-              <div class="table-img-box center-LR center-TB">
-                <img class="table-img" src="../../images/item/200002.jpg" />
-              </div>
-            </td>
-            <td>150</td>
+            <th scope="row">1234568790</th>
+            <td>2024-01-01 00:00:00</td>
+            <td>12</td>
+            <td>1.23kg</td>
             <td>$123456</td>
           </tr>
           <!-- /record -->
