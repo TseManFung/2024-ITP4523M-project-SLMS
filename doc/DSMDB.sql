3bdc73f9c1900efda1f1ac51eb5f348b75d2e5bf
Tse Man Fung <andytse0917@gmail.com>
2024-06-01 20:05:05 +0800

    update data base

--- doc/DSMDB.sql
+++ doc/DSMDB.sql
@@ -1,6 +1,12 @@
-CREATE TABLE `dealer` (
-  `dealerID` integer(10) PRIMARY KEY,
+CREATE TABLE `user` (
+  `userID` integer(10) PRIMARY KEY AUTO_INCREMENT,
   `password` varchar(50) NOT NULL,
+  `salesManagerID` integer(10),
+  `dealerID` integer(10)
+);
+
+CREATE TABLE `dealer` (
+  `dealerID` integer(10) PRIMARY KEY AUTO_INCREMENT,
   `dealerName` varchar(100) UNIQUE NOT NULL,
   `contactName` varchar(100) NOT NULL,
   `contactNumber` integer(30) NOT NULL,
@@ -8,16 +14,22 @@ CREATE TABLE `dealer` (
   `deliveryAddress` varchar(255) NOT NULL
 );
 
+CREATE TABLE `cart` (
+  `userID` integer(10),
+  `sparePartNum` integer(10),
+  `qty` integer(10),
+  PRIMARY KEY (`userID`, `sparePartNum`)
+);
+
 CREATE TABLE `saleManager` (
-  `salesManagerID` integer(10) NOT NULL,
-  `password` varchar(50) NOT NULL,
+  `salesManagerID` integer(10) NOT NULL AUTO_INCREMENT,
   `managerName` varchar(100) UNIQUE NOT NULL,
   `contactName` varchar(100) NOT NULL,
   `contactNumber` integer(30) NOT NULL
 );
 
 CREATE TABLE `order` (
-  `orderID` integer(10) PRIMARY KEY,
+  `orderID` integer(10) PRIMARY KEY AUTO_INCREMENT,
   `orderDateTime` timestamp NOT NULL,
   `deliveryAddress` varchar(255) NOT NULL,
   `deliveryDate` date NOT NULL,
@@ -30,7 +42,7 @@ CREATE TABLE `order` (
 );
 
 CREATE TABLE `spare` (
-  `sparePartNum` integer(10) PRIMARY KEY,
+  `sparePartNum` integer(10) PRIMARY KEY AUTO_INCREMENT,
   `category` Category NOT NULL,
   `sparePartName` varchar(255) NOT NULL,
   `sparePartImage` varchar(100) NOT NULL,
@@ -53,6 +65,14 @@ CREATE TABLE `orderSpare` (
   PRIMARY KEY (`sparePartNum`, `orderID`)
 );
 
+ALTER TABLE `user` ADD FOREIGN KEY (`salesManagerID`) REFERENCES `saleManager` (`salesManagerID`);
+
+ALTER TABLE `user` ADD FOREIGN KEY (`dealerID`) REFERENCES `dealer` (`dealerID`);
+
+ALTER TABLE `cart` ADD FOREIGN KEY (`userID`) REFERENCES `user` (`userID`);
+
+ALTER TABLE `cart` ADD FOREIGN KEY (`sparePartNum`) REFERENCES `spare` (`sparePartName`);
+
 ALTER TABLE `order` ADD FOREIGN KEY (`salesManagerID`) REFERENCES `saleManager` (`salesManagerID`);
 
 ALTER TABLE `order` ADD FOREIGN KEY (`dealerID`) REFERENCES `dealer` (`dealerID`);
@@ -70,8 +90,14 @@ CREATE INDEX `idx_salesManagerID` ON `order` (`salesManagerID`);
 
 CREATE INDEX `idx_orderID` ON `order` (`orderID`);
 
+CREATE INDEX idx_state_order ON `order` (`state`);
+
 CREATE INDEX `idx_sparePartNum_spareQty` ON `spareQty` (`sparePartNum`);
 
 CREATE INDEX `idx_sparePartNum_orderSpare` ON `orderSpare` (`sparePartNum`);
 
 CREATE INDEX `idx_orderID_orderSpare` ON `orderSpare` (`orderID`);
+
+CREATE INDEX idx_salesManagerID_user ON `user` (`salesManagerID`);
+
+CREATE INDEX idx_dealerID_user ON `user` (`dealerID`);
