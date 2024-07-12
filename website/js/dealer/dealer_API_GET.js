e398462940a4a8dd764b6140734f9675b1c4e939
TseManFung <163531908+TseManFung@users.noreply.github.com>
2024-07-13 00:27:29 +0800

    Refactor dealer_API_GET.js for improved readability and error handling

--- website/js/dealer/dealer_API_GET.js
+++ website/js/dealer/dealer_API_GET.js
@@ -1,15 +1,34 @@
-function addToCartqty() {
-    const itemID = "<?php echo $detail['sparePartNum']; ?>";
-    const Qty = document.getElementById('quantityInput').value;
-    $.ajax({
-      type: "POST",
-      url: "./addToCart.php",
-      data: {
-        sparePartNum: itemID,
-        qty: Qty
-      },
-      success: function (data) {
-        location.reload();
+function get_delivery_cost(weight, quantity) {
+  $.ajax({
+    url: `http://127.0.0.1:8080/ship_cost_api/quantity/${quantity}`,
+    success: function (data) {
+      if (data.result == "rejected") {
+        return data.reason;
+      } else if (data.result == "accepted") {
+        quantityCost = data.cost;
+        $.ajax({
+          url: `http://127.0.0.1:8080/ship_cost_api/weight/${weight}`,
+          success: function (data) {
+            if (data.result == "rejected") {
+              return data.reason;
+            } else if (data.result == "accepted") {
+              return Math.max(data.cost, quantityCost);
+            } else {
+              return "Error";
+            }
+          },
+
+          error: function (data) {
+            return data;
+          },
+        });
+      } else {
+        return "Error";
       }
-    });
-  }
+    },
+
+    error: function (data) {
+      return data;
+    },
+  });
+}
